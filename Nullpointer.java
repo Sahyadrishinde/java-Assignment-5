ddfded0228caadd4137f4eb72d35db754758b9cd
Sahyadrishinde <shindesahyadri5@gmail.com>
2025-09-09 17:06:23 +0530

    Add files via upload

---
+++ Nullpointer.java
@@ -0,0 +1,16 @@
+public class Nullpointer{
+
+public static void main (String args[]){
+try{
+ String r=null;
+
+System.out.println("length:"+r.length());
+ }
+catch(Exception e){
+System.out.println(e);
+}
+
+}
+
+
+}
