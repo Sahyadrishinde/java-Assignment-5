a9a38784a82bce0ced57446d4f9b749f478ba315
Sahyadrishinde <shindesahyadri5@gmail.com>
2025-09-09 17:02:39 +0530

    Add files via upload

---
+++ DemoTest.java
@@ -0,0 +1,13 @@
+public class DemoTest
+{
+public static void main(String args[])
+{
+int a,b,c,d;
+a=10;
+b=0;
+c=a/b;
+System.out.println("Division is :"+c);
+d=a+b;
+System.out.println("Addition is :"+d);
+}
+}
